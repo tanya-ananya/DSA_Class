a72feeb011869f71c3fcd7a3b44e510874c44bf5
Taaruni Ananya <106027676+tanya-ananya@users.noreply.github.com>
2025-03-02 20:41:14 -0500

    Add files via upload

---
+++ labs/lab7/CircularSinglyLinkedList.py
@@ -0,0 +1,56 @@
+class Node:
+   def __init__(self, data):
+       self.data = data
+       self.next = None
+
+class CircularSinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_first(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            new_node.next = self.head
+        else:
+            temp = self.head
+            while temp.next != self.head:
+                temp = temp.next
+            new_node.next = self.head
+            temp.next = new_node
+            self.head = new_node
+
+    def insert_last(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            self.head.next = new_node
+        else:
+            temp = self.head
+            while temp.next != self.head:
+                temp = temp.next
+            temp.next = new_node
+            new_node.next = self.head
+
+    def delete_first(self):
+        if not self.head:
+            print('List is empty')
+        elif self.head.next == self.head:
+            self.head = None
+        else:
+            current = self.head
+            while current.next != self.head:
+                current = current.next
+            current.next = self.head.next
+            self.head = self.head.next
+
+    def delete_last(self):
+        if not self.head:
+            print('List is empty')
+        elif self.head.next == self.head:
+            self.head = None
+        else:
+            current = self.head
+            while current.next.next != self.head:
+                current = current.next
+            current.next = self.head
