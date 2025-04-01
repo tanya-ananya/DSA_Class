3faf9f2de10c5a2f90548293b07379880f744671
Taaruni Ananya <106027676+tanya-ananya@users.noreply.github.com>
2025-03-31 21:02:14 -0400

    Add files via upload

---
+++ labs/lab10/PriorityQueue.py
@@ -0,0 +1,24 @@
+import heapq
+
+class PriorityQueue:
+    def __init__(self):
+        self.heap = []
+
+    def insert(self, item):
+        heapq.heappush(self.heap, item)
+
+    def peek(self):
+        if not self.is_empty():
+            return self.heap[0]
+        return None
+
+    def extract(self):
+        if not self.is_empty():
+            return heapq.heappop(self.heap)
+        return None
+
+    def is_empty(self):
+        return len(self.heap) == 0
+
+    def length(self):
+        return len(self.heap)
